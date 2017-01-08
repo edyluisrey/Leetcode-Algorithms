2fa66df24761a434b2192938e9245a01c282a5c0
edyluisrey <eaguirre378@gmail.com>
2017-01-08 17:50:20 -0600

    solution for Two Sum

--- src/leetcode/TwoSum.java
+++ src/leetcode/TwoSum.java
@@ -9,7 +9,7 @@ public class TwoSum {
 	
 	static int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
-         for (int i = 0; i < nums.length - 1; i ++) {
+         for (int i = 0; i < nums.length; i ++) {
             int d = target - nums[i];
             for (int j = i + 1; j < nums.length; j ++) {
                 if (nums[j] == d) {
