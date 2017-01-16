4d2ada3412267e476c6f522306514a059f7e71fb
edyluisrey <eaguirre378@gmail.com>
2017-01-15 22:50:58 -0600

    solution for Longest Common Prefix, format

--- src/leetcode/LongestCommonPrefix.java
+++ src/leetcode/LongestCommonPrefix.java
@@ -7,24 +7,23 @@ public class LongestCommonPrefix {
 		System.out.println(longestCommonPrefix(new String[]{"aabc","aadf","a"}));
 
 	}
+	
 	public static String longestCommonPrefix(String[] strs){
 		if(strs.length==0) return "";
-	    String longest=strs[0];
-	    StringBuilder temp;
-	    for(int i=1;i<strs.length;i++){
-	    	temp = new StringBuilder("");
-	    	for(int j=0;j< longest.length() && j<strs[i].length() ;j++){
-	    		if(longest.charAt(j)==strs[i].charAt(j)){
-	    			temp.append(longest.charAt(j));
-	    		}else
-	    			break;
-	    	}
-	    	if(temp.length()<longest.length())
-	    		longest=temp.toString();
-	    }
-	    return longest;
-			
-		
+		String longest=strs[0];
+		StringBuilder temp;
+		for(int i=1;i<strs.length;i++){
+			temp = new StringBuilder("");
+			for(int j=0;j< longest.length() && j<strs[i].length() ;j++){
+				if(longest.charAt(j)==strs[i].charAt(j)){
+					temp.append(longest.charAt(j));
+				}else
+					break;
+			}
+			if(temp.length()<longest.length())
+				longest=temp.toString();
+		}
+		return longest;					
 	}
 
 }
