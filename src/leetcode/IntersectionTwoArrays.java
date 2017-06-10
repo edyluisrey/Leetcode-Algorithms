c63a3d63231f8bd0645d12b86b6d1f7ac64e22c6
edyluisrey <eaguirre378@gmail.com>
2017-06-09 22:31:46 -0500

    solution for: Intersection of two arrays

--- src/leetcode/IntersectionTwoArrays.java
+++ src/leetcode/IntersectionTwoArrays.java
@@ -14,23 +14,23 @@ public class IntersectionTwoArrays {
 	}
 	
 	public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> set = new HashSet<Integer>();
-        for(int i =0; i < nums1.length; i++){
-           	set.add(nums1[i]);
-        }
-        Set<Integer> set2 = new HashSet<>();
-        for(int i=0; i<nums2.length; i++){
-        	if(set.contains(nums2[i]))
-        		set2.add(nums2[i]);
-        }
-       int[] res = new int[set2.size()];
-       int k=0;
-       Iterator<Integer> it = set2.iterator();
-       while(it.hasNext()){
-    	   res[k]= it.next();
-    	   k++;
-       }
-       return res;
+		    Set<Integer> set = new HashSet<Integer>();
+		    for(int i =0; i < nums1.length; i++){
+		       	set.add(nums1[i]);
+		    }
+		    Set<Integer> set2 = new HashSet<>();
+		    for(int i=0; i<nums2.length; i++){
+		    	if(set.contains(nums2[i]))
+		    		set2.add(nums2[i]);
+		    }
+		    int[] res = new int[set2.size()];
+		    int k=0;
+		    Iterator<Integer> it = set2.iterator();
+		    while(it.hasNext()){
+			   res[k]= it.next();
+			   k++;
+		    }
+		    return res;
     }
 }
 
