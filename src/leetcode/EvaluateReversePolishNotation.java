1fad1628192db5d7f0e324764a5da54d4c58a27d
edyluisrey <eaguirre378@gmail.com>
2017-06-13 17:30:43 -0500

    Solution for: Evaluate reverse polish notation

---
+++ src/leetcode/EvaluateReversePolishNotation.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class EvaluateReversePolishNotation {
+
+	public static void main(String[] args) {
+		EvaluateReversePolishNotation  test= new  EvaluateReversePolishNotation();
+		System.out.println(test.evalRPN(new String[]{"2", "1", "+", "3", "*"}));
+		System.out.println(test.evalRPN(new String[]{"4", "13", "5", "/", "+"}));
+
+	}
+
+	public int evalRPN(String[] tokens) {
+        if(tokens.length==0)  return 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        for(int i=0; i< tokens.length; i++){
+        	if(tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("*")  || tokens[i].equals("/") ){
+        		int re=0;
+        		int temp= stack.pop();
+        		if(tokens[i].equals("+")){
+        		   re= stack.pop()+ temp;
+        		}
+        		if(tokens[i].equals("/")){
+         		   re= stack.pop()/temp;
+         		}
+        		if(tokens[i].equals("*")){
+         		   re= stack.pop()*temp;
+         		}
+        		if(tokens[i].equals("-")){
+         		   re= stack.pop()- temp;
+         		}  
+        		stack.push(re);
+        	}else{
+        		stack.push(Integer.parseInt(tokens[i]));
+        	}
+        }
+        
+        return stack.peek();
+    }
+
+}
