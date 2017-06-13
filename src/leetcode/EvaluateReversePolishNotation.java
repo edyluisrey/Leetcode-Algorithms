967948df86d61c5d5caa1d61516ce361aeadfbdd
edyluisrey <eaguirre378@gmail.com>
2017-06-13 17:33:01 -0500

    Solution for: Evaluate reverse polish notation

--- src/leetcode/EvaluateReversePolishNotation.java
+++ src/leetcode/EvaluateReversePolishNotation.java
@@ -12,31 +12,31 @@ public class EvaluateReversePolishNotation {
 	}
 
 	public int evalRPN(String[] tokens) {
-        if(tokens.length==0)  return 0;
-        Stack<Integer> stack = new Stack<Integer>();
-        for(int i=0; i< tokens.length; i++){
-        	if(tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("*")  || tokens[i].equals("/") ){
-        		int re=0;
-        		int temp= stack.pop();
-        		if(tokens[i].equals("+")){
-        		   re= stack.pop()+ temp;
-        		}
-        		if(tokens[i].equals("/")){
-         		   re= stack.pop()/temp;
-         		}
-        		if(tokens[i].equals("*")){
-         		   re= stack.pop()*temp;
-         		}
-        		if(tokens[i].equals("-")){
-         		   re= stack.pop()- temp;
-         		}  
-        		stack.push(re);
-        	}else{
-        		stack.push(Integer.parseInt(tokens[i]));
-        	}
-        }
-        
-        return stack.peek();
+		if(tokens.length==0)  return 0;
+		Stack<Integer> stack = new Stack<Integer>();
+		for(int i=0; i< tokens.length; i++){
+			if(tokens[i].equals("+") || tokens[i].equals("-") || tokens[i].equals("*")  || tokens[i].equals("/") ){
+				int re=0;
+				int temp= stack.pop();
+				if(tokens[i].equals("+")){
+				   re= stack.pop()+ temp;
+				}
+				if(tokens[i].equals("/")){
+				   re= stack.pop()/temp;
+				}
+				if(tokens[i].equals("*")){
+				   re= stack.pop()*temp;
+				}
+				if(tokens[i].equals("-")){
+			 		   re= stack.pop()- temp;
+			 	}  
+				stack.push(re);
+			}else{
+				stack.push(Integer.parseInt(tokens[i]));
+			}
+		}
+		
+		return stack.peek();
     }
 
 }
