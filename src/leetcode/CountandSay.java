904a488e6cd72ce4aab7bfa111d587d491a35ca0
edyluisrey <eaguirre378@gmail.com>
2017-06-29 17:36:21 -0500

    solution for count and say

---
+++ src/leetcode/CountandSay.java
@@ -0,0 +1,36 @@
+package leetcode;
+
+public class CountandSay {
+
+	public static void main(String[] args) {
+		CountandSay test = new CountandSay();
+		System.out.println(test.countAndSay(5));
+
+	}
+	
+	public String countAndSay(int n) {
+        if(n==0) return "";
+        String prev=String.valueOf(1);
+        for(int i=1;i<n;i++){
+            char[] charPrev= prev.toCharArray();  
+            char prevChar= charPrev[0];  
+            int cont=1;
+            StringBuilder str= new StringBuilder();
+            for(int j=1; j< charPrev.length; j++){
+                if(charPrev[j]==prevChar){
+                	cont++;
+                } 
+                else{
+                    str.append(cont+""+ prevChar);
+                    cont=1;
+                    prevChar=charPrev[j];
+                }
+            }
+            str.append(cont).append(prevChar);
+            prev = str.toString();
+            System.out.println(i+":"+ prev);
+        }
+        return prev;
+    }
+
+}
