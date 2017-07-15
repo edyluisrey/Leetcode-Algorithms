1f1979d9df1101d9ecc5cd14832b39161332e15c
edyluisrey <eaguirre378@gmail.com>
2017-07-14 22:32:55 -0500

    solution for: Partition List

---
+++ src/leetcode/PartitionList.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+public class PartitionList {
+	public ListNode partition(ListNode head, int x) {
+        ListNode list1= new ListNode(0);
+        ListNode list2= new ListNode(0);
+        ListNode temp1= list1;
+        ListNode temp2 = list2;
+        while(head!=null){
+            if(head.val<x){
+                temp1.next= head;
+                temp1= temp1.next;
+            }else{
+                temp2.next = head;
+                temp2 = temp2.next;
+            }
+            head = head.next;
+        }
+        temp2.next=null;
+        temp1.next= list2.next;
+        return list1.next;
+    }
+}
