1152216fc5de8c788af1198543e7645ae529f794
edyluisrey <eaguirre378@gmail.com>
2017-07-19 16:35:06 -0500

    Solution for: Reorder list

--- src/leetcode/ReorderList.java
+++ src/leetcode/ReorderList.java
@@ -2,41 +2,41 @@ package leetcode;
 
 public class ReorderList {
 	public void reorderList(ListNode head) {
-        if (head == null || head.next == null) {
-            return;
-        }
-        ListNode slow = head;
-        ListNode fast= head;
-        while(fast!=null && fast.next!=null){
-            fast= fast.next.next;
-            slow= slow.next;
-        }
-        
-        ListNode second = slow.next;
-		slow.next = null;
-        
-        fast= head;
-        second= reverse(second);
-              
-        while (second != null) {
-			ListNode temp1 = fast.next;
-			ListNode temp2 = second.next;
-            fast.next =second;
-            second.next = temp1;           
-            fast= temp1;
-            second= temp2;
-	  }
-        
-    }
-    
-    public ListNode reverse(ListNode head){
-        ListNode prev=null;
-        while(head!=null){
-            ListNode temp = head.next;
-            head.next= prev;
-            prev= head;
-            head= temp;            
-        }
-        return prev;
-    }
+		    if (head == null || head.next == null) {
+		        return;
+		    }
+		    ListNode slow = head;
+		    ListNode fast= head;
+		    while(fast!=null && fast.next!=null){
+		        fast= fast.next.next;
+		        slow= slow.next;
+		    }
+		    
+		    ListNode second = slow.next;
+			slow.next = null;
+		    
+		    fast= head;
+		    second= reverse(second);
+		          
+		    while (second != null) {
+				ListNode temp1 = fast.next;
+				ListNode temp2 = second.next;
+		        fast.next =second;
+		        second.next = temp1;           
+		        fast= temp1;
+		        second= temp2;
+		  }
+	    
+	}
+	
+	public ListNode reverse(ListNode head){
+		ListNode prev=null;
+		while(head!=null){
+		    ListNode temp = head.next;
+		    head.next= prev;
+		    prev= head;
+		    head= temp;            
+		}
+		return prev;
+	}
 }
