8f71dd1dea2f5a46abb8e399c2998752fee22004
edyluisrey <eaguirre378@gmail.com>
2017-07-19 16:29:03 -0500

    Solution for: LindedList has cyle

---
+++ src/leetcode/LinkedListCycleHas.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+public class LinkedListCycleHas {
+	 public boolean hasCycle(ListNode head) {
+			if(head==null || head.next==null) return false;
+			ListNode slow= head;  
+			ListNode  fast= head;
+			while(fast!=null && fast.next!=null){
+			    fast= fast.next.next;
+			    slow= slow.next;
+			    if(fast==slow) return true;
+				
+			}
+			return false;
+	 }
+}
