ebb6f6de48b801e321747a35d61924f3b59e3a15
edyluisrey <eaguirre378@gmail.com>
2018-01-28 22:19:34 -0500

    public int numJewelsInStones(String J, String S) {

---
+++ src/leetcode2018/JewelsandStones.java
@@ -0,0 +1,20 @@
+package leetcode2018;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class JewelsandStones {
+	
+	public int numJewelsInStones(String J, String S) {
+        int total=0;
+        Set<Character> set =  new HashSet<>();
+        for(char s: J.toCharArray()){
+            set.add(s);
+        }
+        for(char s: S.toCharArray()){
+            if(set.contains(s))
+                total++;
+        }
+        return total;
+    }
+}
