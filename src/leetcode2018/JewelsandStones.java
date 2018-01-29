343bfec7311e045bfcb4dcbccb97fc558a0204ce
edyluisrey <eaguirre378@gmail.com>
2018-01-28 22:22:50 -0500

    Jewels and Stones

--- src/leetcode2018/JewelsandStones.java
+++ src/leetcode2018/JewelsandStones.java
@@ -6,15 +6,15 @@ import java.util.Set;
 public class JewelsandStones {
 	
 	public int numJewelsInStones(String J, String S) {
-        int total=0;
-        Set<Character> set =  new HashSet<>();
-        for(char s: J.toCharArray()){
-            set.add(s);
-        }
-        for(char s: S.toCharArray()){
-            if(set.contains(s))
-                total++;
-        }
-        return total;
+		int total=0;
+		Set<Character> set =  new HashSet<>();
+		for(char s: J.toCharArray()){
+		    set.add(s);
+		}
+		for(char s: S.toCharArray()){
+		    if(set.contains(s))
+		        total++;
+		}
+		return total;
     }
 }
