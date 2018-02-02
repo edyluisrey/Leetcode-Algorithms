f274e0f26aa5459f7d2f45342850babfd6d20b44
edyluisrey <eaguirre378@gmail.com>
2018-02-01 23:29:23 -0500

    Baseball Game

--- src/leetcode2018/BaseballGame.java
+++ src/leetcode2018/BaseballGame.java
@@ -3,36 +3,42 @@ package leetcode2018;
 import java.util.Stack;
 
 public class BaseballGame {
-	public int calPoints(String[] ops) {
-	    Stack<Integer> stack = new Stack<>();
-	    stack.push(0);
-	    int sum=0;
-	    int valid=0;
-	    for(String s: ops) {
-	        if(Character.isDigit(s.charAt(0))) {
-	            stack.push(Integer.parseInt(s));
-	        } else if(s.equals("+")) {
-	            if(stack.size()>1) {
-	                int temp= stack.pop();
-	                valid= temp+ stack.peek();
-	                sum+=valid;
-	                stack.push(temp);
-	                stack.push(valid);
-	            } else { 
-	                sum+=stack.peek();
-	                stack.push(stack.peek());
-	            }
-	                
-	        }else if(s.equals("D")){
-	            valid= stack.peek()*2;
-	            sum+=valid;
-	            stack.push(valid);
-	            
-	        }else if(s.equals("C")){
-	            valid= stack.pop();
-	            sum-=valid;
-	        }
-	    }
-	    return sum;
+	
+	public static void main(String[] args) {
+		calPoints(new String[] {"5","-2","4","C","D","9","+","+"});
+	}
+	
+	public static int calPoints(String[] ops) {
+        Stack<Integer> stack = new Stack<>();
+        int sum=0;
+        int valid=0;
+        for(String s: ops) {
+           if(s.equals("+") && stack.size()>0) {
+                if(stack.size()>1) {
+                    int temp= stack.pop();
+                    valid= temp+ stack.peek();
+                    sum+=valid;
+                    stack.push(temp);
+                    stack.push(valid);
+                } else { 
+                    sum+=stack.peek();
+                    stack.push(stack.peek());
+                }
+                    
+            }else if(s.equals("D") && stack.size()>0){
+                valid= stack.peek()+stack.peek();
+                sum+=valid;
+                stack.push(valid);
+                
+            }else if(s.equals("C") && stack.size()>0){
+                valid= stack.pop();
+                sum-=valid;
+            }else {
+                valid= Integer.parseInt(s);
+                sum+=valid; 
+                stack.push(Integer.parseInt(s));
+            }  
+        }
+        return sum;
     }
 }
