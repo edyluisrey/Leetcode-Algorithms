4f0d6d9610619b108ba280a641674a3a57cafc52
edyluisrey <eaguirre378@gmail.com>
2018-02-01 23:31:25 -0500

    Baseball Game

--- src/leetcode2018/BaseballGame.java
+++ src/leetcode2018/BaseballGame.java
@@ -9,36 +9,36 @@ public class BaseballGame {
 	}
 	
 	public static int calPoints(String[] ops) {
-        Stack<Integer> stack = new Stack<>();
-        int sum=0;
-        int valid=0;
-        for(String s: ops) {
-           if(s.equals("+") && stack.size()>0) {
-                if(stack.size()>1) {
-                    int temp= stack.pop();
-                    valid= temp+ stack.peek();
-                    sum+=valid;
-                    stack.push(temp);
-                    stack.push(valid);
-                } else { 
-                    sum+=stack.peek();
-                    stack.push(stack.peek());
-                }
-                    
-            }else if(s.equals("D") && stack.size()>0){
-                valid= stack.peek()+stack.peek();
-                sum+=valid;
-                stack.push(valid);
-                
-            }else if(s.equals("C") && stack.size()>0){
-                valid= stack.pop();
-                sum-=valid;
-            }else {
-                valid= Integer.parseInt(s);
-                sum+=valid; 
-                stack.push(Integer.parseInt(s));
-            }  
-        }
-        return sum;
+		Stack<Integer> stack = new Stack<>();
+		int sum=0;
+		int valid=0;
+		for(String s: ops) {
+		   if(s.equals("+") && stack.size()>0) {
+		        if(stack.size()>1) {
+		            int temp= stack.pop();
+		            valid= temp+ stack.peek();
+		            sum+=valid;
+		            stack.push(temp);
+		            stack.push(valid);
+		        } else { 
+		            sum+=stack.peek();
+		            stack.push(stack.peek());
+		        }
+		            
+		    }else if(s.equals("D") && stack.size()>0){
+		        valid= stack.peek()+stack.peek();
+		        sum+=valid;
+		        stack.push(valid);
+		        
+		    }else if(s.equals("C") && stack.size()>0){
+		        valid= stack.pop();
+		        sum-=valid;
+		    }else {
+		        valid= Integer.parseInt(s);
+		        sum+=valid; 
+		        stack.push(Integer.parseInt(s));
+		    }  
+		}
+		return sum;
     }
 }
