ce47db865f9b46238ba65f52d43595db06869f24
edyluisrey <eaguirre378@gmail.com>
2018-02-03 20:24:50 -0500

    Toeplitz Matrix

---
+++ src/leetcode2018/ToeplitzMatrix.java
@@ -0,0 +1,16 @@
+package leetcode2018;
+
+public class ToeplitzMatrix {
+	
+	public boolean isToeplitzMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for(int i =0; i< m-1;i++) {
+            for(int j=0; j< n-1; j++) {
+                if(matrix[i][j]!= matrix[i+1][j+1])
+                    return false;
+            }
+        }
+        return true;
+    }
+}
