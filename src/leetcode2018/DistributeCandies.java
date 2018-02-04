8ed89215ad20d6254675edf088f19f9996da8e13
edyluisrey <eaguirre378@gmail.com>
2018-02-03 21:51:57 -0500

    Distribute Candies

--- src/leetcode2018/DistributeCandies.java
+++ src/leetcode2018/DistributeCandies.java
@@ -5,11 +5,15 @@ import java.util.Set;
 
 public class DistributeCandies {
 	
-	public int distributeCandies(int[] candies) {
-	      Set<Integer> set = new HashSet<>();
+	 public int distributeCandies(int[] candies) {
+	      Set<Integer> set =  new HashSet<>();
+	      int  n = candies.length;    
 	      for(int i: candies){
 	          set.add(i);
 	      } 
-	      return set.size();  
-	}
+	      if((set.size()-(n/2))>0){
+	          return n/2;
+	      }else
+	        return set.size(); 
+	 }
 }
