ca61cb5bea3fda32d49c244fcd4e82856addc6c7
edyluisrey <eaguirre378@gmail.com>
2018-02-06 22:46:13 -0500

    Add Strings

--- src/leetcode2018/AddStrings.java
+++ src/leetcode2018/AddStrings.java
@@ -3,50 +3,53 @@ package leetcode2018;
 public class AddStrings {
 	
 	public String addStrings(String num1, String num2) {
-        int rest=0;
-        int n1= num1.length()-1;
-        int n2= num2.length()-1; 
-        StringBuilder str= new StringBuilder();
-        while (n1>=0 && n2>=0) {
-            int a = num1.charAt(n1)-'0';
-            int b = num2.charAt(n2)-'0';
-            int c = a+b+rest;
-            if(c>9){
-                rest = c/10;
-                str.append(c%10);
-            }else{
-                str.append(c);
-            }
-            n1--;
-            n2--;
-        }
-        
-        while(n1>=0){
-            int a = num1.charAt(n1)-'0';
-            int c = a+rest;
-            if(c>9){
-                rest = c/10;
-                str.append(c%10);
-            }else{
-                str.append(c);
-            }
-            n1--;
-        }
-        
-        while(n2>=0){
-            int a = num2.charAt(n2)-'0';
-            int c = a+rest;
-            if(c>9){
-                rest = c/10;
-                str.append(c%10);
-            }else{
-                str.append(c);
-            }
-            n2--;
-        }
-        
-        if(rest>0)
-            str.append(rest);
-        return str.reverse().toString();
+		int rest=0;
+		int n1= num1.length()-1;
+		int n2= num2.length()-1; 
+		StringBuilder str= new StringBuilder();
+		while (n1>=0 && n2>=0) {
+		    int a = num1.charAt(n1)-'0';
+		    int b = num2.charAt(n2)-'0';
+		    int c = a+b+rest;
+		    rest=0;
+		    if(c>9){
+		        rest = c/10;
+		        str.append(c%10);
+		    }else{
+		        str.append(c);
+		    }
+		    n1--;
+		    n2--;
+		}
+		
+		while(n1>=0){
+		    int a = num1.charAt(n1)-'0';
+		    int c = a+rest;
+		    rest=0;
+		    if(c>9){
+		        rest = c/10;
+		        str.append(c%10);
+		    }else{
+		        str.append(c);
+		    }
+		    n1--;
+		}
+		
+		while(n2>=0){
+		    int a = num2.charAt(n2)-'0';
+		    int c = a+rest;
+		    rest=0;
+		    if(c>9){
+		        rest = c/10;
+		        str.append(c%10);
+		    }else{
+		        str.append(c);
+		    }
+		    n2--;
+		}
+		
+		if(rest>0)
+		    str.append(rest);
+		return str.reverse().toString();
     }
 }
