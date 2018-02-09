e8b22a7f9a27d4ffd8024c91834237b2fe102a1a
edyluisrey <eaguirre378@gmail.com>
2018-02-08 23:02:02 -0500

    Relative Ranks

--- src/leetcode2018/RelativeRanks.java
+++ src/leetcode2018/RelativeRanks.java
@@ -17,22 +17,22 @@ public class RelativeRanks {
 		for(int i=0; i< nums.length; i++){
 		    if(i==0){
 		        map.put(nums[n-i], "Gold Medal");
-		    }
-		    else if(i==1){
-		        map.put(nums[n-i], "Silver Medal");
-		    }
-		    else if(i==2){
-		        map.put(nums[n-i], "Bronze Medal");
-		    }
-		    else{
-		        map.put(nums[n-i], (i+1)+"");
+	    }
+	    else if(i==1){
+	        map.put(nums[n-i], "Silver Medal");
+	    }
+	    else if(i==2){
+	        map.put(nums[n-i], "Bronze Medal");
+	    }
+	    else{
+	        map.put(nums[n-i], (i+1)+"");
 		    }
 		}
 		for(int i=0; i< copy.length; i++){
 		    str[i] = map.get(copy[i]); 
 		}
 		return str;
-    }
+	}
     
     public void mergeSort(int[] nums, int l, int p){
         if(l==p){
