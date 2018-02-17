dd4c3c2cfa7775795a5292cce6aa5a6f9a42e332
edyluisrey <eaguirre378@gmail.com>
2018-02-16 22:39:19 -0500

    Longest Palindrome

---
+++ src/leetcode2018/LongestPalindrome.java
@@ -0,0 +1,26 @@
+package leetcode2018;
+
+import java.util.HashMap;
+
+public class LongestPalindrome {
+	public int longestPalindrome(String s) {
+		   HashMap<Character, Integer> map = new HashMap<>();
+		   for(char c: s.toCharArray()){
+			   if(map.containsKey(c)){
+				   map.put(c,map.get(c)+1);
+			   }else{
+				   map.put(c,1);
+			   }
+		   }
+		   int cont=0;
+		   for(char c: map.keySet()){
+			   if(map.get(c)>1){
+				   cont= cont+ map.get(c);
+			   }
+		   }
+		   if(cont<s.toCharArray().length)
+			   cont= cont+1;
+			   
+			return cont;  
+	}
+}
