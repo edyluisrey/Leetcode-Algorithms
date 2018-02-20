be8740a09bf51ef216219108ce459b493f7b7be8
edyluisrey <eaguirre378@gmail.com>
2018-02-19 23:07:26 -0500

    Non-decreasing Array

--- src/leetcode2018/NondecreasingArray.java
+++ src/leetcode2018/NondecreasingArray.java
@@ -2,14 +2,16 @@ package leetcode2018;
 
 public class NondecreasingArray {
 	public boolean checkPossibility(int[] nums) {
-        if(nums.length==0) return true;
-        int n=0; 
-        int temp=nums[0];
-        for(int i = 1; i<  nums.length; i++){
-            if(temp> nums[i]) n++;
-            temp= nums[i];
-            if(n>1) return false;
-        }
-        return true;
+		if(nums.length==0) return true;
+		int n=0; 
+		for(int i = 0; i<  nums.length-1; i++){
+		    if(nums[i] > nums[i+1]){ 
+		        if(n>0) return false;
+		        if (i - 1 >= 0 && i + 2 < nums.length && (nums[i] > nums[i + 2] && nums[i + 1] < nums[i - 1]))
+		            return false;
+		        n++;
+		    }   
+		}
+		return true;
     }
 }
