c1524b5138c52e33090f2deb42c5ddd4b0fac1e3
edyluisrey <eaguirre378@gmail.com>
2018-02-26 21:45:55 -0500

    Count Binary Substrings

--- src/leetcode2018/CountBinarySubstrings.java
+++ src/leetcode2018/CountBinarySubstrings.java
@@ -1,22 +1,21 @@
 package leetcode2018;
 
 public class CountBinarySubstrings {
-
+	
 	 public int countBinarySubstrings(String s) {
 		int t=0;
-		for(int i =0; i< s.length(); i++){
-		    int n=i+1;
-		    while(n< s.length() && s.charAt(n) == s.charAt(i)){
-		       n++;    
-		    }
-		    int m=n+1;
-		    while(m< s.length() && s.charAt(m) == s.charAt(n)){
-		        m++;
-		    }
-		    if((n-i)==(m-n)){
-		        t++;
-		    }            
+		int n=0; 
+		int m=1;
+		for(int i = 1; i< s.length(); i++){  
+		    if(s.charAt(i-1) == s.charAt(i)){
+		       m++;  
+		    }else{
+		        t+= Math.min(n, m);
+		        n= m;
+		        m=1;
+		    }                   
 		}
+		t+= Math.min(n, m);
 		return t;
 	 }
 
