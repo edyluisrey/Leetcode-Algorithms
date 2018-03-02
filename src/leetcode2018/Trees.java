4ea04e479bdceb72eb7b5c16d95fa9fe2365ca71
edyluisrey <eaguirre378@gmail.com>
2018-03-01 22:48:13 -0500

    Trees

--- src/leetcode2018/Trees.java
+++ src/leetcode2018/Trees.java
@@ -95,6 +95,13 @@ public class Trees {
 		TreeNode re = left!=null? left: right;       
 		return re;
 	}
+	
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null)  return true;
+        if(p==null || q==null)  return false;
+        if(p.val!=q.val)  return false;
+        return  isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
 
 }
 
