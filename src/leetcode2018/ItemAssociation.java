389c7998b8bd524ef6533e09a0692725f2a4d9cd
edyluisrey <eaguirre378@gmail.com>
2018-03-04 18:54:34 -0500

    ItemAssociation

--- src/leetcode2018/ItemAssociation.java
+++ src/leetcode2018/ItemAssociation.java
@@ -17,33 +17,33 @@ public class ItemAssociation {
 	}
 	
 	public String[] largestItemAssociation(String[][] itemAssociation)
-	{   StringBuffer temp= new StringBuffer();
-	    int n=Integer.MIN_VALUE;
-		Queue<String> q = new LinkedList<>();
-		for(int i = 0; i< itemAssociation.length; i++){
-		     if(!q.isEmpty() && q.peek()!=itemAssociation[i][0]){
-		    	 if(n<q.size()){
-		    		 n=q.size(); 
-			    	 temp.setLength(0);
-			         while(!q.isEmpty()){
-			             temp.append(q.poll()+",");
+	{      StringBuffer temp= new StringBuffer();
+		    int n=Integer.MIN_VALUE;
+			Queue<String> q = new LinkedList<>();
+			for(int i = 0; i< itemAssociation.length; i++){
+			     if(!q.isEmpty() && q.peek()!=itemAssociation[i][0]){
+			    	 if(n<q.size()){
+			    		 n=q.size(); 
+				    	 temp.setLength(0);
+				         while(!q.isEmpty()){
+				             temp.append(q.poll()+",");
 			         }
-			     }    
-		     }
-		     for(int j=0;j< itemAssociation[0].length;j++){  	
+			       }    
+		         }
+		        for(int j=0;j< itemAssociation[0].length;j++){  	
 		    	    if(!q.contains(itemAssociation[i][j]))
 		                 q.offer(itemAssociation[i][j]);
-		     }
-		}
-		if(n<q.size()){
-		 n=q.size(); 
-	    	 temp.setLength(0);
-	         while(!q.isEmpty()){
-	             temp.append(q.poll()+",");
-	         }
-	     } 
-		String[] re= temp.toString().split(",");
-		return re;
+		       }
+			}
+			if(n<q.size()){
+			 n=q.size(); 
+				 temp.setLength(0);
+			     while(!q.isEmpty()){
+			         temp.append(q.poll()+",");
+			     }
+			 } 
+			String[] re= temp.toString().split(",");
+			return re;
 	}
 
 }
