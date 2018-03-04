fc02950a2b8a274f3986de8fca2c7563a4b53e4a
edyluisrey <eaguirre378@gmail.com>
2018-03-04 18:57:15 -0500

    ItemAssociation

--- src/leetcode2018/ItemAssociation.java
+++ src/leetcode2018/ItemAssociation.java
@@ -8,7 +8,7 @@ public class ItemAssociation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ItemAssociation test = new ItemAssociation();
-		String[][] mat= new String[][]{{"Item1","Item2"},
+		String[][] mat= new String[][]{ {"Item1","Item2"},
 										{"Item3","Item4"},
 										{"Item4","Item5"}};
 										
@@ -17,33 +17,34 @@ public class ItemAssociation {
 	}
 	
 	public String[] largestItemAssociation(String[][] itemAssociation)
-	{      StringBuffer temp= new StringBuffer();
-		    int n=Integer.MIN_VALUE;
-			Queue<String> q = new LinkedList<>();
-			for(int i = 0; i< itemAssociation.length; i++){
-			     if(!q.isEmpty() && q.peek()!=itemAssociation[i][0]){
-			    	 if(n<q.size()){
-			    		 n=q.size(); 
-				    	 temp.setLength(0);
-				         while(!q.isEmpty()){
-				             temp.append(q.poll()+",");
-			         }
-			       }    
+	{      
+		StringBuffer temp= new StringBuffer();
+		int n=Integer.MIN_VALUE;
+		Queue<String> q = new LinkedList<>();
+		for(int i = 0; i< itemAssociation.length; i++){
+		     if(!q.isEmpty() && q.peek()!=itemAssociation[i][0]){
+		    	 if(n<q.size()){
+		    		 n=q.size(); 
+			    	 temp.setLength(0);
+			         while(!q.isEmpty()){
+			             temp.append(q.poll()+",");
 		         }
-		        for(int j=0;j< itemAssociation[0].length;j++){  	
-		    	    if(!q.contains(itemAssociation[i][j]))
-		                 q.offer(itemAssociation[i][j]);
-		       }
-			}
-			if(n<q.size()){
-			 n=q.size(); 
-				 temp.setLength(0);
-			     while(!q.isEmpty()){
-			         temp.append(q.poll()+",");
-			     }
-			 } 
-			String[] re= temp.toString().split(",");
-			return re;
+		       }    
+		     }
+		    for(int j=0;j< itemAssociation[0].length;j++){  	
+			    if(!q.contains(itemAssociation[i][j]))
+		             q.offer(itemAssociation[i][j]);
+		   }
+		}
+		if(n<q.size()){
+		 n=q.size(); 
+			 temp.setLength(0);
+		     while(!q.isEmpty()){
+		         temp.append(q.poll()+",");
+		     }
+		 } 
+		String[] re= temp.toString().split(",");
+		return re;
 	}
 
 }
