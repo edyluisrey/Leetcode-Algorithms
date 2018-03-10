aec31a7996ac68cd4ed6ff6e111d11c51a72ca8a
edyluisrey <eaguirre378@gmail.com>
2018-03-09 22:50:52 -0500

    Tree - hasPathSum

--- src/leetcode2018/Trees.java
+++ src/leetcode2018/Trees.java
@@ -5,6 +5,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+import amazon.TreeNode;
+
 
 public class Trees {
 
@@ -132,6 +134,16 @@ public class Trees {
 	             re.add(level);
 	        }
 	        return re;
+	 }
+	 
+	 public boolean hasPathSum(TreeNode root, int sum) {
+			if(root==null) return false;  
+			System.out.println(root.val);
+			if(root.left==null && root.right==null) {
+				System.out.println(root.val +" ="+ sum);
+				return sum == root.val;
+			}
+			return  hasPathSum(root.left, sum-root.val) || hasPathSum(root.right,sum-root.val);
 	 }
 	 
 }
